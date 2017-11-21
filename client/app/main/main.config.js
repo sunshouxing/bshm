72a57e057b24666c5a6d6030a8c3521debba03ca
Arthur <shouxing.sun@aliyun.com>
2017-11-21 08:48:46 +0800

    Translate apps' nav.

--- client/app/main/main.config.js
+++ client/app/main/main.config.js
@@ -1,12 +1,16 @@
 'use strict';
 
-export default function(msNavigationServiceProvider) {
+export default function($translatePartialLoaderProvider, msNavigationServiceProvider) {
   'ngInject';
+
+  // translation
+  $translatePartialLoaderProvider.addPart('app/main');
 
   // register apps navigation group
   msNavigationServiceProvider.saveItem('apps', {
     title: 'APPS',
     group: true,
+    translate: 'MAIN.APPS_NAV',
     weight: 1
   });
 }
