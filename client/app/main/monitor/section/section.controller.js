ae0b63a560d679602c5236dad07733e411ad897b
shouxing.sun <shouxing.sun@aliyun.com>
2018-05-21 16:06:22 +0800

    Complete section monitor's framework.

--- client/app/main/monitor/section/section.controller.js
+++ client/app/main/monitor/section/section.controller.js
@@ -8,10 +8,39 @@ export default class SectionMonitor {
   /*********************
    *      Methods      *
    *********************/
-  constructor() {}
+  constructor($state) {
+    'ngInject';
+
+    this.$state = $state;
+  }
 
   $onInit() {
-    this.test = 123;
+    this.chart = {
+      config: {
+        theme: 'default',
+        dataLoaded: false
+      },
+      option: {
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        title: {
+          text: '阜长线分离式立交主梁截面三数据视图',
+          textStyle: {
+            color: 'rgba(18, 89, 147, 0.75)',
+            fontSize: 18,
+            fontWeight: 'bold'
+          }
+        },
+        legend: {
+          show: true,
+          selectedMode: 'multiple',
+          orient: 'vertical',
+          left: 10,
+          bottom: 10
+        },
+        animation: false,
+        dataset: dataset
+      }
+    };
   }
 }
 
--- client/app/main/monitor/section/section.module.js
+++ client/app/main/monitor/section/section.module.js
@@ -2,10 +2,12 @@
 
 import angular from 'angular';
 
+import config from './section.config';
 import SectionMonitor from './section.controller';
 
 export default angular
   .module('app.monitor.section', [])
+  .config(config)
   .controller('SectionMonitor', SectionMonitor)
   .name;
 
