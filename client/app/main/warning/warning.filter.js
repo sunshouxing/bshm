100564da6e02ef2e40267eaba389fc81fc3d0af5
shouxing.sun <shouxing.sun@aliyun.com>
2018-05-23 16:05:56 +0800

    Add i18n support to warning level and status filter.

--- client/app/main/warning/warning.filter.js
+++ client/app/main/warning/warning.filter.js
@@ -1,14 +1,22 @@
 'use strict';
 
-export function warningLevelFilter() {
-  return num => {
-    return ['黄色预警', '红色预警'][num];
-  };
+export function warningLevelFilter($translate) {
+  'ngInject';
+
+  return num => [
+    $translate.instant('WARNING.YELLOW_WARNING'),
+    $translate.instant('WARNING.RED_WARNING')
+  ][num];
 }
 
-export function warningStatusFilter() {
-  return num => {
-    return ['未处理', '处理中', '已处理'][num];
-  };
+export function warningStatusFilter($translate) {
+  'ngInject';
+
+  return num => [
+    $translate.instant('WARNING.UNHANDLED'),
+    $translate.instant('WARNING.HANDLING'),
+    $translate.instant('WARNING.HANDLED')
+  ][num];
 }
 
+/* vim:set sw=2 ts=2 sts=2: */
